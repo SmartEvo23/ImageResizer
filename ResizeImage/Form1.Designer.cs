40572d205a235868f3b97495bf6080b843540fa5
Kiss Edvard <edvardsystem@gmail.com>
2023-01-31 16:18:16 +0200

    Another part

--- ResizeImage/Form1.Designer.cs
+++ ResizeImage/Form1.Designer.cs
@@ -29,14 +29,34 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelLanguage = new System.Windows.Forms.Label();
             this.comboBoxLanguage = new System.Windows.Forms.ComboBox();
+            this.selectImage = new System.Windows.Forms.Label();
+            this.textBoxSelect = new System.Windows.Forms.TextBox();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.saveImage = new System.Windows.Forms.Label();
+            this.textBoxSave = new System.Windows.Forms.TextBox();
+            this.buttonSave1 = new System.Windows.Forms.Button();
+            this.checkBoxAutoResize = new System.Windows.Forms.CheckBox();
+            this.labelRatio = new System.Windows.Forms.Label();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.labelType = new System.Windows.Forms.Label();
+            this.comboBoxFileType = new System.Windows.Forms.ComboBox();
+            this.labelHelp = new System.Windows.Forms.Label();
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.buttonResize = new System.Windows.Forms.Button();
+            this.buttonSave2 = new System.Windows.Forms.Button();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
-            // label1
+            // labelLanguage
             // 
-            resources.ApplyResources(this.label1, "label1");
-            this.label1.Name = "label1";
+            resources.ApplyResources(this.labelLanguage, "labelLanguage");
+            this.labelLanguage.Name = "labelLanguage";
             // 
             // comboBoxLanguage
             // 
@@ -45,12 +65,147 @@
             this.comboBoxLanguage.Name = "comboBoxLanguage";
             this.comboBoxLanguage.SelectedIndexChanged += new System.EventHandler(this.comboBoxLanguage_SelectedIndexChanged);
             // 
+            // selectImage
+            // 
+            resources.ApplyResources(this.selectImage, "selectImage");
+            this.selectImage.Name = "selectImage";
+            this.selectImage.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBoxSelect
+            // 
+            resources.ApplyResources(this.textBoxSelect, "textBoxSelect");
+            this.textBoxSelect.Name = "textBoxSelect";
+            // 
+            // buttonSelect
+            // 
+            resources.ApplyResources(this.buttonSelect, "buttonSelect");
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // saveImage
+            // 
+            resources.ApplyResources(this.saveImage, "saveImage");
+            this.saveImage.Name = "saveImage";
+            // 
+            // textBoxSave
+            // 
+            resources.ApplyResources(this.textBoxSave, "textBoxSave");
+            this.textBoxSave.Name = "textBoxSave";
+            // 
+            // buttonSave1
+            // 
+            resources.ApplyResources(this.buttonSave1, "buttonSave1");
+            this.buttonSave1.Name = "buttonSave1";
+            this.buttonSave1.UseVisualStyleBackColor = true;
+            this.buttonSave1.Click += new System.EventHandler(this.buttonSave1_Click);
+            // 
+            // checkBoxAutoResize
+            // 
+            resources.ApplyResources(this.checkBoxAutoResize, "checkBoxAutoResize");
+            this.checkBoxAutoResize.Checked = true;
+            this.checkBoxAutoResize.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAutoResize.Name = "checkBoxAutoResize";
+            this.checkBoxAutoResize.UseVisualStyleBackColor = true;
+            this.checkBoxAutoResize.CheckedChanged += new System.EventHandler(this.checkBoxAutoResize_CheckedChanged);
+            // 
+            // labelRatio
+            // 
+            resources.ApplyResources(this.labelRatio, "labelRatio");
+            this.labelRatio.Name = "labelRatio";
+            // 
+            // labelWidth
+            // 
+            resources.ApplyResources(this.labelWidth, "labelWidth");
+            this.labelWidth.Name = "labelWidth";
+            // 
+            // textBoxWidth
+            // 
+            resources.ApplyResources(this.textBoxWidth, "textBoxWidth");
+            this.textBoxWidth.Name = "textBoxWidth";
+            // 
+            // labelHeight
+            // 
+            resources.ApplyResources(this.labelHeight, "labelHeight");
+            this.labelHeight.Name = "labelHeight";
+            // 
+            // textBoxHeight
+            // 
+            resources.ApplyResources(this.textBoxHeight, "textBoxHeight");
+            this.textBoxHeight.Name = "textBoxHeight";
+            // 
+            // labelType
+            // 
+            resources.ApplyResources(this.labelType, "labelType");
+            this.labelType.Name = "labelType";
+            // 
+            // comboBoxFileType
+            // 
+            resources.ApplyResources(this.comboBoxFileType, "comboBoxFileType");
+            this.comboBoxFileType.FormattingEnabled = true;
+            this.comboBoxFileType.Name = "comboBoxFileType";
+            // 
+            // labelHelp
+            // 
+            resources.ApplyResources(this.labelHelp, "labelHelp");
+            this.labelHelp.Name = "labelHelp";
+            // 
+            // buttonShow
+            // 
+            resources.ApplyResources(this.buttonShow, "buttonShow");
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            // 
+            // buttonResize
+            // 
+            resources.ApplyResources(this.buttonResize, "buttonResize");
+            this.buttonResize.Name = "buttonResize";
+            this.buttonResize.UseVisualStyleBackColor = true;
+            this.buttonResize.Click += new System.EventHandler(this.buttonResize_Click);
+            // 
+            // buttonSave2
+            // 
+            resources.ApplyResources(this.buttonSave2, "buttonSave2");
+            this.buttonSave2.Name = "buttonSave2";
+            this.buttonSave2.UseVisualStyleBackColor = true;
+            this.buttonSave2.Click += new System.EventHandler(this.buttonSave2_Click);
+            // 
+            // labelProgress
+            // 
+            resources.ApplyResources(this.labelProgress, "labelProgress");
+            this.labelProgress.Name = "labelProgress";
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            // 
             // Form1
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelProgress);
+            this.Controls.Add(this.buttonSave2);
+            this.Controls.Add(this.buttonResize);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.labelHelp);
+            this.Controls.Add(this.comboBoxFileType);
+            this.Controls.Add(this.labelType);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.labelHeight);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.labelRatio);
+            this.Controls.Add(this.checkBoxAutoResize);
+            this.Controls.Add(this.buttonSave1);
+            this.Controls.Add(this.textBoxSave);
+            this.Controls.Add(this.saveImage);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.textBoxSelect);
+            this.Controls.Add(this.selectImage);
             this.Controls.Add(this.comboBoxLanguage);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelLanguage);
             this.Name = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -59,7 +214,27 @@
 
         #endregion
 
-        private Label label1;
+        private Label labelLanguage;
         private ComboBox comboBoxLanguage;
+        private Label selectImage;
+        private TextBox textBoxSelect;
+        private Button buttonSelect;
+        private Label saveImage;
+        private TextBox textBoxSave;
+        private Button buttonSave1;
+        private CheckBox checkBoxAutoResize;
+        private Label labelRatio;
+        private Label labelWidth;
+        private TextBox textBoxWidth;
+        private Label labelHeight;
+        private TextBox textBoxHeight;
+        private Label labelType;
+        private ComboBox comboBoxFileType;
+        private Label labelHelp;
+        private Button buttonShow;
+        private Button buttonResize;
+        private Button buttonSave2;
+        private Label labelProgress;
+        private ProgressBar progressBar1;
     }
 }
--- ResizeImage/Form1.cs
+++ ResizeImage/Form1.cs
@@ -6,6 +6,12 @@ namespace ResizeImage
 
     public partial class Form1 : Form
     {
+
+        Image img;
+        string[] fileType = { ".png", ".jpeg", ".jpg", ".gif" };
+        int imgWidth;
+        int imgHeight;
+
         public Form1()
         {
             InitializeComponent();
@@ -13,17 +19,19 @@ namespace ResizeImage
             comboBoxLanguage.Items.Add("Romana");
             comboBoxLanguage.SelectedItem = null;
 
-            Image img;
-            string[] fileType = { ".png", ".jpeg", ".jpg", ".gif" };
-            int imgWidth;
-            int imgHeight;
+            textBoxWidth.Enabled = false;
+            textBoxHeight.Enabled = false;
+            labelWidth.Enabled = false;
+            labelHeight.Enabled = false;
+            labelType.Enabled = false;
+            comboBoxFileType.Enabled = false;
         }
 
         private void comboBoxLanguage_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (comboBoxLanguage.SelectedItem.ToString() == "English")
             {
-                ChangeLanguage("en");
+                ChangeLanguage("en-US");
             }
             else if (comboBoxLanguage.SelectedItem.ToString() == "Romana")
             {
@@ -31,7 +39,7 @@ namespace ResizeImage
             }
             else
             {
-                ChangeLanguage("en-EN");
+                ChangeLanguage("en-US");
             }
         }
 
@@ -41,6 +49,100 @@ namespace ResizeImage
             {
                 ComponentResourceManager resources = new ComponentResourceManager(typeof(Form1));
                 resources.ApplyResources(c, c.Name, new CultureInfo(lang));
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "images | *.png;*.jpg;*.jpeg;*.gif";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBoxSelect.Text = ofd.FileName;
+                img = Image.FromFile(ofd.FileName);
+            }
+        }
+
+        private void buttonSave1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBoxSave.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void checkBoxAutoResize_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxAutoResize.Checked) 
+            {
+                textBoxWidth.Enabled = false;
+                textBoxHeight.Enabled = false;
+                labelWidth.Enabled = false;
+                labelHeight.Enabled = false;
+                labelType.Enabled = false;
+                comboBoxFileType.Enabled = false;
+            }
+            else
+            {
+                textBoxWidth.Enabled = true;
+                textBoxHeight.Enabled = true;
+                labelWidth.Enabled = true;
+                labelHeight.Enabled = true;
+                labelType.Enabled = true;
+                comboBoxFileType.Enabled = true;
+            }
+        }
+
+        //Resize without center and crop
+        static Image Resize(Image image, int w, int h)
+        {
+            Bitmap bmp = new Bitmap(w, h);
+            Graphics graphics = Graphics.FromImage(bmp);
+            graphics.DrawImage(image, 0, 0, w, h);
+            graphics.Dispose();
+
+            return bmp;
+        }
+
+        private void buttonResize_Click(object sender, EventArgs e)
+        {
+            if (textBoxSelect.Text != null && textBoxSave.Text != null)
+            {
+                int w = Convert.ToInt32(textBoxWidth.Text);
+                int h = Convert.ToInt32(textBoxHeight.Text);
+                img = Resize(img, w, h);
+                ((Button)sender).Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Selectati imaginea si alegeti zona de salvare!");
+            }
+
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+
+            for (var i = 0; i < 100; i++) progressBar1.Value++;
+        }
+
+        private void buttonSave2_Click(object sender, EventArgs e)
+        {
+            int dot = 0; 
+            int slash = 0;
+            for (int j = textBoxSelect.Text.Length - 1; j >= 0; j--)
+            {
+                if (textBoxSelect.Text[j] == '.')
+                    dot = j;
+                else if (textBoxSelect.Text[j] == '\\')
+                {
+                    slash = j;
+                    break;
+                }
             }
         }
     }
